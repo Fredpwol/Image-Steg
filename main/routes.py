e70f4f64538931131e2ac3e83680722672b88d9b
Fredpwol <cyborg290@gmail.com>
2021-04-23 12:36:25 +0100

    added form to encode route

--- main/routes.py
+++ main/routes.py
@@ -1,12 +1,17 @@
 from main import app
+from flask import request, flash, redirect, render_template
+from main.form import EncodeForm, DecodeForm
 
 @app.route("/home")
 def home():
-    return " home"
+    return render_template("index.html")
 
-@app.route("/encode")
+@app.route("/encode", ["GET", "POST"])
 def encode_image():
-    return "Encode Page!"
+    form = EncodeForm()
+    if form.validate_on_submit():
+        return redirect("home")
+    return "Encode!!"
 
 @app.route("/decode")
 def decode_image():
