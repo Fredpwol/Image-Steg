a2db92bdd42f9c4b63e2137ade49c5e8fc919c2b
Fredpwol <cyborg290@gmail.com>
2021-04-23 13:17:14 +0100

    added config and routes

--- main/__init__.py
+++ main/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
-
+from main import config
 
 app = Flask(__name__)
+app.config.from_object(config.Development)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+from main import routes
