d43a94d20a48672dc3727562be44a615df16e927
Fredpwol <cyborg290@gmail.com>
2021-04-25 22:10:12 +0100

    removed unused configs

--- main/config.py
+++ main/config.py
@@ -5,30 +5,6 @@ from datetime import timedelta
 class Development:
     DEBUG = True
     SECRET_KEY = "changeme"
-
-    # Skipping the below configuration would trigger a 500
-    # on bad api request with no auth headers case
-    # Read more at flask jwt extended issue 86
-    # https://github.com/vimalloc/flask-jwt-extended/issues/86
-    PROPAGATE_EXCEPTIONS = True
-
-    JWT_ACCESS_TOKEN_EXPIRES = False
-
-    SQLALCHEMY_DATABASE_URI = "sqlite:///stego.db"
-
-    # Suppress SQLALCHEMY_TRACK_MODIFICATIONS overhead warning
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    TMP_UPLOAD_PATH = '/tmp'
-
-    ENC_IMG_SAVE_PATH = 'images'
-    APP_ROOT = os.path.dirname(
-        os.path.dirname(
-            os.path.abspath(__file__)
-        )
-    )
-    HOST_DOMAIN = 'http://localhost:8000'
-
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
@@ -41,29 +17,12 @@ class Development:
 class Production(Development):
     DEBUG = False
     SECRET_KEY = ""
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'SQLALCHEMY_DATABASE_URI',
-        'postgresql://localhost/stegano'
-    )
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(7)
-
-    ENC_IMG_SAVE_PATH = 'images'
-    HOST_DOMAIN = 'https://stegano.sreenadh.me/'
 
 
 class Testing(Development):
     DEBUG = True
     TESTING = True
     SECRET_KEY = "testsecret"
-    JWT_ACCESS_TOKEN_EXPIRES = False
-
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres'\
-        '@localhost/stegano_test'
-
-    # Suppress SQLALCHEMY_TRACK_MODIFICATIONS overhead warning
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    ENC_IMG_SAVE_PATH = 'images'
 
 
 configuration = {
