42e6bc9c60bd6cfe6dec746c0f2b64ff053284af
Emmzy17 <emmanueladjerry@gmail.com>
2022-11-10 17:09:59 +0100

    first commit

--- main/__init__.py
+++ main/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from main import config
-from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
-Bootstrap(app)
+
 app.config.from_object(config.Development)
 
 
--- main/routes.py
+++ main/routes.py
@@ -13,21 +13,31 @@ def check_image_path():
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    form = EncodeForm(request.form)
-    deform = DecodeForm(request.form)
-
-    if form.validate_on_submit():
+    encode_form = EncodeForm()
+    decode_form = DecodeForm()
+    
+    #if form.validate_on_submit():
+    #    pass
         
     
-    if request.method.lower() == "post":
-        print(form.is_submitted(), "submitted", form.validate(),)
-        print ("Submitted!!")
-        f = form.file.data
-        filename = secure_filename(f.filename)
+    #if request.method.lower() == "post":
+        #print(form.is_submitted(), "submitted", form.validate(),)
+        #print ("Submitted!!")
+    if encode_form.validate_on_submit():
+        encode_f = encode_form.file.data
+        print(encode_f)
+        filename = secure_filename(encode_f.filename)
         print("Path", os.path.join(app.root_path, "images", filename))
-        return redirect("/")
-        f.save()
-    return render_template("base.html", form=form, deform = deform)
+        encode_f.save()
+        return redirect("/encode")
+    if decode_form.validate_on_submit():
+        decode_f = decode_form.file.data
+        filename = secure_filename(decode_f.filename)
+        print(request.files)
+        decode_f.save()
+        
+
+    return render_template("base.html", encode_form=encode_form, decode_form = decode_form)
 
 @app.route("/encode", methods=["GET", "POST"])
 def encode_image():
