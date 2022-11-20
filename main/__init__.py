06558a5593072be92bd67b53b42f8d53c4cadc19
Fredpwol <fredpwol@gmail.com>
2022-11-20 02:19:32 +0100

    init socket

--- main/__init__.py
+++ main/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from main import config
 from flask_bootstrap import Bootstrap
 from flask_socketio import SocketIO
+from settings import CELERY_MESSAGING_QUEUE, SOCKET_IO_MESSAGING_QUEUE
 
 app = Flask(__name__)
 Bootstrap(app)
 app.config.from_object(config.Development)
-io = SocketIO(app)
+
+io = SocketIO(app, message_queue=SOCKET_IO_MESSAGING_QUEUE, async_mode="gevent")
 
 from main import routes
 from service import io_listeners
